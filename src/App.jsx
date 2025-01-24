85cbba1260aaeb88b3234d3ff8f5a08c27df10ad
Nouran <noransharawna@hotmail.com>
2025-01-24 09:40:31 +0300

    React Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Login from "./pages/Login.jsx";
+import NewUsers from "./pages/Users.jsx";
+import Settings from "./pages/Settings.jsx";
+import Account from "./pages/Account.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to={"dashboard"} />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<NewUsers />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 1rem;
+  padding: 1.2rem 1.6rem;
+`;
+
+export default Input;
