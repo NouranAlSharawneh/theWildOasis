42db7901c621bb6844695562d25c9094bbd48275
Nouran <noransharawna@hotmail.com>
2025-02-18 13:13:04 +0300

    Error Boundary Added

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,16 @@
 import Button from "../../ui/Button";
+import { useCheckOut } from "./useCheckOut.js";
 
 function CheckoutButton({ bookingId }) {
+  const { mutate: checkout, isPending } = useCheckOut();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isPending}
+    >
       Check out
     </Button>
   );
